41ea652c29b835fa0302515d6b695836ab7e6ca7
Enock <enochwanjala.97@gmail.com>
2023-10-03 10:24:36 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = (num1 * num2)
+console.log(multiply)
+
+
+//generate random
+
+let random=Math.floor(Math.random()*10+1);
+
+//Reminder equals 4
+let num3 = 460;
+let num4 = 76;
+let mod= (num3%num4)
+console.log(mod)
+
+//Returns 20 as highest number
+let num5 = 20;
+let num6 = 10;
+let num7 = 15;
+let max = Math.max(num5,num6,num7)
+console.log(max)
